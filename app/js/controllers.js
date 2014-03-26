296f903882751e5fe2574b6561711f1540c8d225
ahinkojosemalespin <jose.malespin@ahinko.com>
2014-03-26 12:07:28 -0600

    Adding step 4

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,14 +7,17 @@ var phonecatApp = angular.module('phonecatApp', []);
 phonecatApp.controller('PhoneListCtrl', function($scope) {
   $scope.phones = [
     {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
+     'snippet': 'Fast just got faster with Nexus S.',
+     'age': 1},
     {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
+     'snippet': 'The Next, Next Generation tablet.',
+     'age': 2},
     {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'},
+     'snippet': 'The Next, Next Generation tablet.',
+     'age': 3},
     {'name': 'Alcatel TPOP',
-     'snippet': 'A simple android phone'},
-    {'name': 'Nokia X',
-     'snippet': 'The first Nokia phone with android!'}
+     'snippet': 'An simple android phone',
+     'age': 4}
   ];
+
 });
--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -12,27 +12,31 @@ describe('PhoneCat App', function() {
 
 
     it('should filter the phone list as user types into the search box', function() {
-      expect(repeater('.phones li').count()).toBe(5);
+      expect(repeater('.phones li').count()).toBe(4);
 
       input('query').enter('nexus');
       expect(repeater('.phones li').count()).toBe(1);
 
       input('query').enter('motorola');
       expect(repeater('.phones li').count()).toBe(2);
+
+      input('query').enter('alcatel');
+      expect(repeater('.phones li').count()).toBe(1);
     });
 
 
-    it('should display the current filter value within an element with id "status"',
-        function() {
-      expect(element('#status').text()).toMatch(/You're actually searching for: \s*$/);
-   
-      input('query').enter('nexus');
-   
-      expect(element('#status').text()).toMatch(/You're actually searching for: nexus\s*$/);
-   
-      //alternative version of the last assertion that tests just the value of the binding
-      //using('#status').expect(binding('query')).toBe('nexus');
-    });
+    it('should be possible to control phone order via the drop down select box', function() {
+      input('query').enter('tablet'); //let's narrow the dataset to make the test assertions shorter
 
+      expect(repeater('.phones li', 'Phone List').column('phone.name')).
+          toEqual(["Motorola XOOM\u2122 with Wi-Fi",
+                   "MOTOROLA XOOM\u2122"]);
+
+      select('orderProp').option('Alphabetical');
+
+      expect(repeater('.phones li', 'Phone List').column('phone.name')).
+          toEqual(["MOTOROLA XOOM\u2122",
+                   "Motorola XOOM\u2122 with Wi-Fi"]);
+    });
   });
 });
